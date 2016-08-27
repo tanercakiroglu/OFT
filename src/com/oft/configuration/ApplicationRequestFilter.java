cc0f88cd4674b9cb63d20502d40844dd6809e07c
tanercakiroglu <tanercakiroglu@gmail.com>
2016-08-27 17:45:51 +0300

    request filter added

---
+++ src/com/oft/configuration/ApplicationRequestFilter.java
@@ -0,0 +1,29 @@
+package com.oft.configuration;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.ws.rs.core.Context;
+
+import com.sun.jersey.spi.container.ContainerRequest;
+import com.sun.jersey.spi.container.ContainerRequestFilter;
+
+public class ApplicationRequestFilter  implements ContainerRequestFilter{
+
+	@Context
+	private HttpServletRequest servletRequest;
+	
+	@Override
+	public ContainerRequest filter(ContainerRequest request) {
+		
+		String ipAddress = request.getHeaderValue("X-FORWARDED-FOR");
+		if (ipAddress == null) {
+			   ipAddress = servletRequest.getRemoteAddr();
+		}
+		
+		if("/OFT/user/getuser".equalsIgnoreCase(request.getRequestUri().getPath())){
+			
+			System.out.println("sadasd");
+		}
+		return request;
+	}
+
+}
