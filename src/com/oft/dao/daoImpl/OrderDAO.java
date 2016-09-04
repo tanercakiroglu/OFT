8efa2e942bdd85cda2af751dad3e627084939350
tanercakiroglu <tanercakiroglu@gmail.com>
2016-09-04 14:25:21 +0300

    orderdetail changed

--- src/com/oft/dao/daoImpl/OrderDAO.java
+++ src/com/oft/dao/daoImpl/OrderDAO.java
@@ -18,8 +18,7 @@ import com.oft.util.Util;
 
 public class OrderDAO extends BaseJdbcDAO implements IOrderDAO {
 
-	
-	private final RowMapper<MenuPojo> rowMapperMenuDetail = new RowMapper<MenuPojo>(){
+	private final RowMapper<MenuPojo> rowMapperMenuDetail = new RowMapper<MenuPojo>() {
 
 		@Override
 		public MenuPojo mapRow(ResultSet rs, int rowNum) throws SQLException {
@@ -32,101 +31,97 @@ public class OrderDAO extends BaseJdbcDAO implements IOrderDAO {
 			menu.setDescEN(rs.getString("DESCRIPTION_EN"));
 			menu.setNameEN(rs.getString("ITEM_NAME_EN"));
 			menu.setItemCount(rs.getInt("ITEM_COUNT"));
-			
+
 			return menu;
 		}
-		 
-	 };
-		private final RowMapper<OrderPojo> rowMapperOrder = new RowMapper<OrderPojo>(){
 
-			@Override
-			public OrderPojo mapRow(ResultSet rs, int rowNum) throws SQLException {
-				OrderPojo order = new OrderPojo();
-				order.setID(rs.getString("ID"));
-				return order;
-			}
-		 };
-		 
-			private final RowMapper<OrderDetailPojo> rowMapperOrderDetail = new RowMapper<OrderDetailPojo>(){
+	};
+	private final RowMapper<OrderPojo> rowMapperOrder = new RowMapper<OrderPojo>() {
 
-				@Override
-				public OrderDetailPojo mapRow(ResultSet rs, int rowNum) throws SQLException {
-					OrderDetailPojo orderDetail = new OrderDetailPojo();
-					orderDetail.setOrderID(rs.getString("ID"));
-					orderDetail.setItemCount(rs.getInt("ITEM_COUNT"));
-					orderDetail.setItemID(rs.getInt("ITEM_ID"));
-					return orderDetail;
-				}
-			 };
-	
+		@Override
+		public OrderPojo mapRow(ResultSet rs, int rowNum) throws SQLException {
+			OrderPojo order = new OrderPojo();
+			order.setID(rs.getString("ID"));
+			return order;
+		}
+	};
+
+	private final RowMapper<OrderDetailPojo> rowMapperOrderDetail = new RowMapper<OrderDetailPojo>() {
+
+		@Override
+		public OrderDetailPojo mapRow(ResultSet rs, int rowNum)
+				throws SQLException {
+			OrderDetailPojo orderDetail = new OrderDetailPojo();
+			orderDetail.setOrderID(rs.getString("ID"));
+			orderDetail.setItemCount(rs.getInt("ITEM_COUNT"));
+			orderDetail.setItemID(rs.getInt("ITEM_ID"));
+			return orderDetail;
+		}
+	};
+
 	@Override
-	public int addOrderDetail(List<OrderPojo> orderList,String orderID) {
-		
-		int isRowAffected=0;
+	public int addOrderDetail(List<OrderPojo> orderList, String orderID) {
+
+		int isRowAffected = 0;
 		for (OrderPojo order : orderList) {
-		if(controlExistingOrderDetail(orderID,order.getItemID())==null){
-				String query = "INSERT INTO oft_order_detail (ORDER_ID,ITEM_ID,ITEM_COUNT) VALUES (:orderID,:itemID,:itemCount)";
-				Map<String, Object> namedParameters = new HashMap<String, Object>();
-				namedParameters.put("itemID", order.getItemID());
-				namedParameters.put("itemCount", order.getItemCount());
-				namedParameters.put("orderID", orderID);
-				namedParameterJdbcTemplate.update(query, namedParameters);
-				isRowAffected++;
+			String query =null;
+			Map<String, Object> namedParameters = new HashMap<String, Object>();
+			if (controlExistingOrderDetail(orderID, order.getItemID()) == null) {
+				 query = "INSERT INTO oft_order_detail (ORDER_ID,ITEM_ID,ITEM_COUNT) VALUES (:orderID,:itemID,:itemCount)";
+			} else {
+				 query = "update oft_order_detail set ITEM_COUNT=ITEM_COUNT + :itemCount WHERE ORDER_ID=:orderID and ITEM_ID=:itemID";
+			}
+			namedParameters.put("itemID", order.getItemID());
+			namedParameters.put("itemCount", order.getItemCount());
+			namedParameters.put("orderID", orderID);
+			namedParameterJdbcTemplate.update(query, namedParameters);
+			isRowAffected++;
 		}
-		else {
-				String query = "update oft_order_detail set ITEM_COUNT=ITEM_COUNT + :itemCount WHERE ORDER_ID=:orderID and ITEM_ID=:itemID";
-				Map<String, Object> namedParameters = new HashMap<String, Object>();
-				namedParameters.put("itemID", order.getItemID());
-				namedParameters.put("itemCount", order.getItemCount());
-				namedParameters.put("orderID", orderID);
-				namedParameterJdbcTemplate.update(query, namedParameters);
-				isRowAffected++;
-		}
-		} 
 		return isRowAffected;
 	}
 
-
 	@Override
 	public List<MenuPojo> getOrder(int roomNO) {
-	
+
 		String query = "SELECT  oft.item_count, me.ITEM_NAME_EN,  me.ITEM_NAME,  me.PRICE_IN_DOLAR,  me.PRICE_IN_YTL,  me.PRICE_IN_EURO,"
-				+ " me.DESCRIPTION,  me.DESCRIPTION_EN FROM OFT_ORDER o,  menu me,  oft_order_detail oft " 
-				+ " WHERE  oft.ORDER_ID=o.ID " 
+				+ " me.DESCRIPTION,  me.DESCRIPTION_EN FROM OFT_ORDER o,  menu me,  oft_order_detail oft "
+				+ " WHERE  oft.ORDER_ID=o.ID "
 				+ " AND oft.order_id = (SELECT id FROM oft_order WHERE room_no=:roomNO AND session_status=:sessionStatus )"
 				+ " AND me.ID=oft.ITEM_ID";
 		Map<String, Object> parameters = new HashMap<String, Object>();
 		parameters.put("roomNO", roomNO);
 		parameters.put("sessionStatus", 1);
-		List<MenuPojo> orderDetail = namedParameterJdbcTemplate.query(query,parameters, rowMapperMenuDetail);
+		List<MenuPojo> orderDetail = namedParameterJdbcTemplate.query(query,
+				parameters, rowMapperMenuDetail);
 		return orderDetail;
 	}
-	
 
 	@Override
 	public int closeOrder(int roomNO) {
-		
-		int rowAffected =0;
+
+		int rowAffected = 0;
 		String query = "UPDATE oft_order set session_Status=:sessionStatus, order_session_closed_date=:timestamp  where ROOM_NO=:roomNO";
-		Map<String,Object> parameters= new HashMap<String, Object>();
-		parameters.put("roomNO",roomNO);
+		Map<String, Object> parameters = new HashMap<String, Object>();
+		parameters.put("roomNO", roomNO);
 		parameters.put("timestamp", Util.getCurrentTimeStamp());
-		parameters.put("sessionStatus",0);
-		rowAffected=namedParameterJdbcTemplate.update(query, parameters);
+		parameters.put("sessionStatus", 0);
+		rowAffected = namedParameterJdbcTemplate.update(query, parameters);
 		return rowAffected;
 	}
-	
-	public OrderPojo controlExistingOpenOrder(int roomNO) throws BusinessException{
-	
+
+	public OrderPojo controlExistingOpenOrder(int roomNO)
+			throws BusinessException {
+
 		String query = "SELECT * from oft_order WHERE ROOM_NO=:roomNO and SESSION_STATUS=:sessionStatus";
-		Map<String,Object> parameters= new HashMap<String, Object>();
-		parameters.put("roomNO",roomNO);
+		Map<String, Object> parameters = new HashMap<String, Object>();
+		parameters.put("roomNO", roomNO);
 		parameters.put("sessionStatus", 1);
-			
-		List<OrderPojo> order =  namedParameterJdbcTemplate.query(query, parameters,rowMapperOrder);
-		if(order!=null && order.size()==1)
+
+		List<OrderPojo> order = namedParameterJdbcTemplate.query(query,
+				parameters, rowMapperOrder);
+		if (order != null && order.size() == 1)
 			return order.get(0);
-		else if(order==null || order.size()==0)
+		else if (order == null || order.size() == 0)
 			return null;
 		else
 			throw new BusinessException("hacı database karışık soyleyim");
@@ -134,71 +129,68 @@ public class OrderDAO extends BaseJdbcDAO implements IOrderDAO {
 
 	@Override
 	public int addOrder(int roomNO) {
-	
-		int rowAffected =0;
+
+		int rowAffected = 0;
 		String query = "INSERT INTO  oft_order (ROOM_NO) VALUES(:roomNO)";
-		Map<String,Object> parameters= new HashMap<String, Object>();
-		parameters.put("roomNO",roomNO);
-		rowAffected=namedParameterJdbcTemplate.update(query, parameters);
+		Map<String, Object> parameters = new HashMap<String, Object>();
+		parameters.put("roomNO", roomNO);
+		rowAffected = namedParameterJdbcTemplate.update(query, parameters);
 		return rowAffected;
 	}
 
-
 	@Override
-	public int controlDeleteCount(int itemID, String orderID) throws BusinessException {
-		
+	public int controlDeleteCount(int itemID, String orderID)
+			throws BusinessException {
+
 		String query = "select * from oft_order_detail where item_id =:itemID and order_id=:orderID";
-		Map<String,Object> parameters= new HashMap<String, Object>();
+		Map<String, Object> parameters = new HashMap<String, Object>();
 		System.out.println(itemID + Integer.parseInt(orderID));
-		parameters.put("itemID",itemID);
+		parameters.put("itemID", itemID);
 		parameters.put("orderID", Integer.parseInt(orderID));
-		List<OrderDetailPojo> orderDetail =  namedParameterJdbcTemplate.query(query, parameters,rowMapperOrderDetail);
-		if(orderDetail!=null && orderDetail.size()==1)
+		List<OrderDetailPojo> orderDetail = namedParameterJdbcTemplate.query(
+				query, parameters, rowMapperOrderDetail);
+		if (orderDetail != null && orderDetail.size() == 1)
 			return orderDetail.get(0).getItemCount();
 		else
 			throw new BusinessException("hacı database karışık soyleyim");
 	}
-	
+
 	@Override
-	public int deleteOrderDetail(int itemID,String orderID) {
-		
+	public int deleteOrderDetail(int itemID, String orderID) {
+
 		int rowAffected = 0;
 		String query = "DELETE FROM oft_order_detail WHERE ORDER_ID=:orderID and ITEM_ID=:itemID";
 		Map<String, Object> namedParameters = new HashMap<String, Object>();
-		namedParameters.put("orderID",orderID);
+		namedParameters.put("orderID", orderID);
 		namedParameters.put("itemID", itemID);
-		rowAffected=namedParameterJdbcTemplate.update(query, namedParameters);
-		return rowAffected ;
+		rowAffected = namedParameterJdbcTemplate.update(query, namedParameters);
+		return rowAffected;
 	}
-	
+
 	@Override
 	public int decreaseItemCount(int itemID, String orderID) {
-		
+
 		int rowAffected = 0;
 		String query = "UPDATE  oft_order_detail SET ITEM_COUNT=ITEM_COUNT-1 WHERE ORDER_ID=:orderID and ITEM_ID=:itemID";
 		Map<String, Object> namedParameters = new HashMap<String, Object>();
-		namedParameters.put("orderID",orderID);
+		namedParameters.put("orderID", orderID);
 		namedParameters.put("itemID", itemID);
-		rowAffected=namedParameterJdbcTemplate.update(query, namedParameters);
-		return rowAffected ;
+		rowAffected = namedParameterJdbcTemplate.update(query, namedParameters);
+		return rowAffected;
 	}
-	
-	private String controlExistingOrderDetail(String orderID,int itemID){
-		
+
+	private String controlExistingOrderDetail(String orderID, int itemID) {
+
 		String query = "select * from oft_order_detail where ORDER_ID =:orderID and ITEM_ID=:itemID";
 		Map<String, Object> namedParameters = new HashMap<String, Object>();
-		namedParameters.put("orderID",orderID);
+		namedParameters.put("orderID", orderID);
 		namedParameters.put("itemID", itemID);
-		List<OrderDetailPojo> list=namedParameterJdbcTemplate.query(query, namedParameters,rowMapperOrderDetail);
-	    if(list!= null && list.size()==1)
-	    	return list.get(0).getOrderID();
-	    return null;
-		
-		
+		List<OrderDetailPojo> list = namedParameterJdbcTemplate.query(query,
+				namedParameters, rowMapperOrderDetail);
+		if (list != null && list.size() == 1)
+			return list.get(0).getOrderID();
+		return null;
+
 	}
-	
-	
+
 }
-
-
-
